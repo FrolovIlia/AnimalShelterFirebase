d9393c80bfb762393485782ec11c3259a6e7c799
FrolovIlia <ilia_frolov@list.ru>
2025-04-06 20:54:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/animalshelterfirebase/data/Animal.kt
@@ -0,0 +1,10 @@
+package com.example.animalshelterfirebase.data
+
+
+data class Animal(
+    val name: String = "",
+    val description: String = "",
+    val age: String = "",
+    val category: String = "",
+    val imageUrl: String = ""
+)
