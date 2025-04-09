2fee7bd8aeb540831fbe1c586ba84974c3e21eb4
FrolovIlia <ilia_frolov@list.ru>
2025-04-09 20:50:36 +0300

    Create auth form

--- app/src/main/java/com/example/animalshelterfirebase/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/animalshelterfirebase/ui/login/LoginScreen.kt
@@ -1,11 +1,16 @@
 package com.example.animalshelterfirebase.ui.login
 
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,14 +19,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.animalshelterfirebase.R
+import com.example.animalshelterfirebase.ui.theme.BoxFilterColor
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 @Composable
 fun LoginScreen() {
-    val auth = Firebase.auth
+//    val auth = Firebase.auth
 
     val emailState = remember {
         mutableStateOf("")
@@ -30,42 +44,121 @@ fun LoginScreen() {
         mutableStateOf("")
     }
 
+    Image(
+        painter = painterResource(
+            id = R.drawable.dog
+        ),
+        contentDescription = "BG",
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop,
+    )
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(BoxFilterColor)
+
+
+    )
 
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(
+                start = 50.dp,
+                end = 50.dp
+            ),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        TextField(value = emailState.value, onValueChange = {
+
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "logo",
+            modifier = Modifier
+                .size(150.dp)
+        )
+        Spacer(modifier = Modifier.size(10.dp))
+        Text(
+            text = "Animal Shelter App",
+            color = Color.White,
+            fontWeight = FontWeight.Bold,
+            fontFamily = FontFamily.Serif,
+            fontSize = 30.sp
+        )
+
+        Spacer(modifier = Modifier.size(10.dp))
+        RoundedCornerTextField(
+            text = emailState.value,
+            label = "Email"
+        ) {
             emailState.value = it
-        })
-        Spacer(modifier = Modifier.height(10.dp))
-
-        TextField(value = passwordState.value, onValueChange = {
-            passwordState.value = it
-        })
-        Spacer(modifier = Modifier.height(10.dp))
-
-        Button(onClick = {
-            signUp(auth, emailState.value, passwordState.value)
-        }) {
-            Text(text = "Sigh Up")
         }
+        Spacer(modifier = Modifier.height(10.dp))
+
+        RoundedCornerTextField(
+            text = passwordState.value,
+            label = "Password"
+        ) {
+            passwordState.value = it
+        }
+
+        Spacer(modifier = Modifier.height(10.dp))
+
+        LoginButton(text = "Sign In") { }
+
+        LoginButton(text = "Sign Up") { }
+
+
     }
 }
 
 private fun signUp(auth: FirebaseAuth, email: String, password: String) {
     auth.createUserWithEmailAndPassword(email, password)
         .addOnCompleteListener { task ->
-            if (task.isSuccessful){
+            if (task.isSuccessful) {
                 Log.d("Mylog", "Sign Up successful")
             } else {
                 Log.d("Mylog", "Sign Up failure!")
+            }
+        }
+}
+
+
+private fun signIn(auth: FirebaseAuth, email: String, password: String) {
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                Log.d("Mylog", "Sign In successful")
+            } else {
+                Log.d("Mylog", "Sign In failure!")
             }
 
         }
 }
 
 
+private fun signOut(auth: FirebaseAuth) {
+    auth.signOut()
+}
+
+private fun deleteAccount(auth: FirebaseAuth, email: String, password: String) {
+    val credential = EmailAuthProvider.getCredential(email, password)
+
+    auth.currentUser?.reauthenticate(credential)?.addOnCompleteListener {
+        if (it.isSuccessful) {
+            auth.currentUser?.delete()?.addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.d("Mylog", "Account was deleted!")
+                } else {
+                    Log.d("Mylog", "Failure delete account!")
+                }
+            }
+        } else {
+            Log.d("Mylog", "Failure re-authenticate!")
+
+        }
+    }
+}
 
 
---
+++ app/src/main/java/com/example/animalshelterfirebase/ui/login/RoundedCornerTextField.kt
@@ -0,0 +1,43 @@
+package com.example.animalshelterfirebase.ui.login
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.animalshelterfirebase.ui.theme.ButtonColor
+
+
+@Composable
+fun RoundedCornerTextField(
+    text: String,
+    label: String,
+    onValueChange: (String) -> Unit
+) {
+
+    TextField(
+        value = text,
+        onValueChange = {
+            onValueChange(it)
+        },
+        shape = RoundedCornerShape(25.dp),
+        colors = TextFieldDefaults.colors(
+            focusedContainerColor = Color.White,
+            unfocusedContainerColor = Color.LightGray,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(1.dp, ButtonColor, RoundedCornerShape(25.dp)),
+        label = {
+            Text(text = label, color = Color.Gray)
+        },
+        singleLine = true
+    )
+}
